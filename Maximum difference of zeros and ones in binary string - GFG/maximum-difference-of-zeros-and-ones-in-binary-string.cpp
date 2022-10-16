f10f7330dabd1750ed0708c04157d31dc1242432
neerajsandal <79656311+neerajsandal@users.noreply.github.com>
2022-10-16 21:58:36 +0530

    Added solution - LeetHub

--- Maximum difference of zeros and ones in binary string - GFG/maximum-difference-of-zeros-and-ones-in-binary-string.cpp
+++ Maximum difference of zeros and ones in binary string - GFG/maximum-difference-of-zeros-and-ones-in-binary-string.cpp
@@ -9,37 +9,32 @@ class Solution{
 public:	
 	int maxSubstring(string S)
 	{
-	    int x, sum = 0;
-	    int count = 0;
-	    int table[S.size()] = {0};
-	    if(S[0] == '1'){
+	    int n = S.size();
+	    int table[n];
+	    int sum = 0, x = 0;
+	    if(S[0] == '0'){
+	        table[0] = 1;
+	        sum = 1;
+	    }
+	    else {
 	        table[0] = -1;
 	        sum = -1;
 	    }
-	    else{
-	        table[0] = 1;
-	        sum = 1;
-	        count++;
-	    }
-	    for(int i = 1; i < S.size(); i++){
+	    
+	    for(int i = 1; i < n; i++){
 	        if(S[i] == '0'){
 	            x = 1;
-	            count++;
-	        }
-	        else if(S[i] == '1'){
+	        }else{
 	            x = -1;
 	        }
-	        if(sum <= 0)
-	        sum = x;
+	        if(sum == -1){
+	            sum = x;
+	        }
 	        else
 	        sum += x;
-	        
 	        table[i] = max(sum, table[i-1]);
 	    }
-	    if(count == 0){
-	        return -1;
-	    }
-	    return table[S.size()-1];
+	    return table[n-1];
 	}
 };
 
