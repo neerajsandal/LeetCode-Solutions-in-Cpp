95f57df0bc49c38495706a198c57b83e7e4c2cae
neerajsandal <79656311+neerajsandal@users.noreply.github.com>
2023-03-26 14:29:25 +0530

    Added solution - LeetHub

---
+++ Pascal Triangle - GFG/pascal-triangle.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+#define ll long long
+
+// } Driver Code Ends
+// #define ll long long
+class Solution{
+public:
+    vector<ll> nthRowOfPascalTriangle(int n) {
+        vector<ll> v(n, 1);
+        for(int i = 1; i < n-1; i++){
+            for(int j = i; j > 0; j--){
+                v[j] += v[j-1];
+                v[j] %= 1000000007;
+            }
+        }
+        return v;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+
+void printAns(vector<ll> &ans) {
+    for (auto &x : ans) {
+        cout << x << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        auto ans = ob.nthRowOfPascalTriangle(n);
+        printAns(ans);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
