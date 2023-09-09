c792667ed7efe731f480dfdc98118831b4df9c04
neerajsandal <79656311+neerajsandal@users.noreply.github.com>
2023-09-09 15:01:16 +0530

    Added solution - LeetHub

--- Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
+++ Minimum number of deletions and insertions. - GFG/minimum-number-of-deletions-and-insertions..cpp
@@ -5,31 +5,34 @@ using namespace std;
 // } Driver Code Ends
 class Solution{
 		
-int lcs(string str, string str1)
-{
-    int n = str.size();
-    int m = str1.size();
-    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
-    for(int i = 1; i <= n; i++)
-    {
-        for(int j = 1; j <= m; j++)
-        {
-            if(str[i-1] == str1[j-1]){
-                dp[i][j] = 1+dp[i-1][j-1];
-            }
-            else{
-                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-            }
-        }
-    }
-    return  dp[n][m];
-}
+
 	public:
+	int lcs(string str1, string str2)
+	{
+	    int n = str1.size();
+	    int m = str2.size();
+	    
+	    vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+	    for(int i = 1; i <= n; i++)
+	    {
+	        for(int j = 1; j <= m; j++)
+	        {
+	            if(str1[i-1] == str2[j-1]){
+	                dp[i][j] = 1 + dp[i-1][j-1];
+	            }
+	            else{
+	                dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+	            }
+	        }
+	    }
+	    return dp[n][m];
+	}
 	int minOperations(string str1, string str2) 
 	{ 
-	   int n = str1.size();
-	   int m = str2.size();
-	   return (n+m) - (2*lcs(str1, str2));
+	    int k = lcs(str1, str2);
+	    int deletions = str1.size()-k;
+	    int insertions = str2.size()-k;
+	    return insertions + deletions;
 	} 
 };
 
