a8ba02dd5cf7f5d05a36e279b023fa9e12c511ca
neerajsandal <79656311+neerajsandal@users.noreply.github.com>
2023-09-12 10:04:45 +0530

    Added solution - LeetHub

--- Number of Coins - GFG/number-of-coins.cpp
+++ Number of Coins - GFG/number-of-coins.cpp
@@ -6,33 +6,26 @@ using namespace std;
 class Solution{
 
 	public:
-	int minCoins(int coins[], int n, int amount) 
+	int helper(int coins[], int i, int V, vector<vector<int>> &dp)
+	{
+	    if(i == 0){
+	        if(V%coins[i] == 0) return V/coins[i];
+	        else return 1e9;
+	    }
+	    if(dp[i][V] != -1) return dp[i][V];
+	    int notake = helper(coins, i-1, V, dp);
+	    int take = 1e9;
+	    if(V - coins[i] >= 0){
+	        take = 1 + helper(coins, i, V-coins[i], dp);
+	    }
+	    return dp[i][V] = min(take, notake);
+	}
+	int minCoins(int coins[], int M, int V) 
 	{ 
-        vector<vector<int>> dp(n, vector<int>(amount+1, -1));
-        for(int i = 0; i <= amount; i++)
-        {
-            if(i % coins[0] == 0) 
-                dp[0][i] = i / coins[0];
-            else 
-                dp[0][i] = 1e9;
-        }
-
-        for(int i = 1; i < n; i++)
-        {
-            for(int j = 0; j <= amount; j++)
-            {
-                int nottake = dp[i-1][j];
-                int take = INT_MAX;
-                if(coins[i] <= j)
-                {
-                    take = 1+dp[i][j-coins[i]];
-                }
-                dp[i][j] = min(take, nottake);
-            }
-        }
-        int ans = dp[n-1][amount];
-        if(ans >= 1e9) return -1;
-        else return ans;
+	    vector<vector<int>> dp(M, vector<int>(V+1, -1));
+	    int ans = helper(coins, M-1, V, dp);
+	    if(ans >= 1e9) return -1;
+	    return ans;
 	} 
 	  
 };
